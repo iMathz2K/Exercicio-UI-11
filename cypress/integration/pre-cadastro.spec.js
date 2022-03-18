322aa7acf5e5606d0027ad95a8237822f0d75a9a
Matheus Castro <matheus_ccastro@yahoo.com.br>
2022-03-18 11:39:11 -0300

    meu primeiro commit

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import { internet } from 'faker';
 
 describe('Funcionalidade Pré Cadastro', () => {
 
@@ -8,9 +9,14 @@ beforeEach(() => {
 });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type('teste_aluno2122@teste.com')
+        cy.get('#reg_email').type(internet.email())
         cy.get('#reg_password').type('!teste@teste$')
         cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type('Matheus')
+        cy.get('#account_last_name').type('Costa')
+        cy.get('.woocommerce-Button').click()
        
     });
     
